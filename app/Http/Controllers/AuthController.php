1782aa1ec2280f83ce0380cbe809dbe0c7cc02c2
Ani <perusicani@gmail.com>
2022-01-30 22:31:47 +0100

    - CSRF token CORS issue

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,7 +13,7 @@ class AuthController extends Controller
         $fields = $request->validate([
             'name' => 'required|string',
             'email' => 'required|string|unique:users,email',
-            'password' => 'required|string|confirmed',
+            'password' => 'required|string|min:8|confirmed',
         ]);
 
         $user = User::create([
@@ -22,47 +22,49 @@ class AuthController extends Controller
             'password' => bcrypt($fields['password']),
         ]);
 
-        $token = $user->createToken('myapptoken')->plainTextToken;
+        $token = $user->createToken($user->email.'_Token')->plainTextToken;
 
         $response = [
             'user' => $user,
             'token' => $token,
+            'message' => 'Registered successfully',
         ];
-
+        
         return response($response, 200);    //everything good
     }
-
+    
     public function login(Request $request) {
         $fields = $request->validate([     
             'email' => 'required|string',
             'password' => 'required|string',
         ]);
-
+        
         //check email
         $user = User::where('email', $fields['email'])->first();
-
+        
         //check pass
         if (!$user || !Hash::check($fields['password'], $user->password)) {
             return response([
                 'message' => 'Bad credentials!'
             ], 401);
         }
-
-        $token = $user->createToken('myapptoken')->plainTextToken;
-
+        
+        $token = $user->createToken($user->email.'_Token')->plainTextToken;
+        
         $response = [
             'user' => $user,
             'token' => $token,
+            'message' => 'Logged in successfully',
         ];
-
+        
         return response($response, 201);    //successfully created code
     }
-
+    
     public function logout(Request $request) {
         auth()->user()->tokens()->delete();
-
+        
         return [
-            'message' => 'Logged out'
+            'message' => 'Logged out', 
         ];
     }
 }
